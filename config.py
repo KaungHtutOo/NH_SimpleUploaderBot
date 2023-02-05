e19e63996a256543fbb7051e79551a1e8ac9c7b8
KaungHtutOo <73459374+KaungHtutOo@users.noreply.github.com>
2023-02-05 22:37:48 +0630

    Update config.py

--- config.py
+++ config.py
@@ -2,16 +2,16 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5869348525:AAEB4Ge3mm5G1Er4rjvZIwiUyZxtlzN0IIs")
     # The Telegram API things
-    API_ID = int(os.environ.get("API_ID", 12345))
-    API_HASH = os.environ.get("API_HASH")
+    API_ID = int(os.environ.get("API_ID", 7978114))
+    API_HASH = os.environ.get("API_HASH", "5f7839feeba133497f24acfd005ef2ec")
     # Get these values from my.telegram.org
     # the download location, where the HTTP Server runs
     DOWNLOAD_LOCATION = "./DOWNLOADS"
     # Telegram maximum file upload size
     MAX_FILE_SIZE = 50000000
-    TG_MAX_FILE_SIZE = 2097152000
+    TG_MAX_FILE_SIZE = 4194304000 #2097152000
     FREE_USER_MAX_FILE_SIZE = 50000000
     VIDEO_FORMATS = ["mp4", "mkv", "mov", "m4v", "avi", "unknown_video"]
     AUDIO_FORMATS = ["mp3", "flac", "m4a", "wav"]
@@ -26,8 +26,8 @@ class Config(object):
     # set timeout for subprocess
     PROCESS_MAX_TIMEOUT = 3600
     # your telegram id
-    OWNER_ID = int(os.environ.get("OWNER_ID", ""))
-    SESSION_NAME = "SIMPLEUPLOADERBOT"
+    OWNER_ID = int(os.environ.get("OWNER_ID", "1760280598"))
+    SESSION_NAME = "NH_Temporary_Bot"
     # database uri (mongodb)
-    DATABASE_URL = os.environ.get("DATABASE_URL", "")
+    DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://Multi:MultiBot@mutlti.9wqqaki.mongodb.net/?retryWrites=true&w=majority")
     MAX_RESULTS = "50"
